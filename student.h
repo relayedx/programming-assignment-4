2d89a240f9c2fe201784b6939072be5323db229a
keyroseh <65471405+keyroseh@users.noreply.github.com>
2025-09-24 13:20:17 -0700

    Update student.h

--- student.h
+++ student.h
@@ -1,19 +1,59 @@
-#include <iostream>
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <stdio.h>
 #include <string>
 using namespace std;
 
+const int MAX_PROGRAMS =6;
+const int MAX_TESTS =4;
 
-class Student {
-private: 
-  int studentId;
-  int p[6];
-  int t[4];
-  int f[1];
-  string lastname;
-  string firstname;
+class Student{
+private:
+    //Student Identifyers
+    string firstName, lastName;
+    int studentNumber;
 
+    //Grades
+    double programs[MAX_PROGRAMS];
+    double tests[MAX_TESTS];
+    double finalExam;
+    
 public:
-  Student(int id, string ln, string fn) : lastname(ln), firstname(fn), studentId(id) {}
+    Student(); // Default constructor
+    Student(const string& last, const string& first, int id); // Name
 
+
+    // Student Data (Used by choice C and O)
+    void setName(const string& last, const string& first);
+    void setStudentNumber(int id);
+    const string& getFirstName() const;
+    const string& getLastName() const;
+    int getStudentNumber() const;
+
+    // P). Program Grades
+    void setProgramGrade(int i, double grade); // i = index
+    
+    // T). Test Grades
+    void setTestGrade(int i, double grade); // i = index
+
+    // F). Final Exam Grades
+    void setFinalExamGrade(double grade);
+
+    // Calculations
+    double programAverage(int programs) const; // Programming assignment average
+    double testAverage(int tests) const; // Exam average
+    double finalGrade(int programmingW, int testsW, int finalW, int programs, int tests); //Weighted final grade, W = weight
+
+    //O). Output Student/Grade Data
+    void printInfo(ofstream& out, int programs, int tests, int finals, int programmingW, int testsW, int finalW) const;
+    
+    //Q. Quit (in main)
+    
+    // File functions
+    void writeDat(ofstream& out, int programs, int tests, int finals) const;
+    void readDat(ifstream& in, int programs, int tests, int finals) const;
 
 };
+
+#endif // STUDENT_H
